5fafcae2c1a88215a989079f202f52ef994a3ad3
Alex Denissov <adenissov@pivotal.io>
2018-10-31 17:08:07 -0700

    WIP: Using copy.c:BeginCopy in pxfBeginForeignScan, deleted duplicated code

--- contrib/pxf_fdw/sql/pxf_fdw_test4.sql
+++ contrib/pxf_fdw/sql/pxf_fdw_test4.sql
@@ -10,4 +10,4 @@ OPTIONS ("fs.s3a.awsAccessKeyId" 'your access key', "fs.s3a.awsSecretAccessKey" 
 
 CREATE FOREIGN TABLE demo_table (first TEXT, last TEXT)
 SERVER demoserver
-OPTIONS (location 'pxf://tmp/tmp?FRAGMENTER=org.greenplum.pxf.api.examples.DemoFragmenter&ACCESSOR=org.greenplum.pxf.api.examples.DemoAccessor&RESOLVER=org.greenplum.pxf.api.examples.DemoTextResolver');
+OPTIONS (format 'text', location 'pxf://tmp/tmp?FRAGMENTER=org.greenplum.pxf.api.examples.DemoFragmenter&ACCESSOR=org.greenplum.pxf.api.examples.DemoAccessor&RESOLVER=org.greenplum.pxf.api.examples.DemoTextResolver');
